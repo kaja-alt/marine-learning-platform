3cf73929f928b24569da971179b1058688d9d04e
Kaja Solend <kajasolend@Kajas-MacBook-Air.local>
2026-07-03 09:02:33 +0200

    Fix trainer button on published PWA

---
+++ website/website/service-worker.js
@@ -0,0 +1,24 @@
+const CACHE_NAME = 'marine-platform-legacy-website-v12';
+
+self.addEventListener('install', event => {
+  event.waitUntil(self.skipWaiting());
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys()
+      .then(keys => Promise.all(keys.filter(key => /^(marine-platform|marineTraining|marine-learning)/.test(key) && key !== CACHE_NAME).map(key => caches.delete(key))))
+      .then(() => self.clients.claim())
+      .then(() => self.clients.matchAll({ type: 'window' }))
+      .then(clients => {
+        const target = new URL('../index.html?v=12', self.registration.scope).toString();
+        return Promise.all(clients.map(client => client.navigate(target)));
+      })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  if (event.request.mode === 'navigate') {
+    event.respondWith(Response.redirect(new URL('../index.html?v=12', self.registration.scope).toString(), 302));
+  }
+});
